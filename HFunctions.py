397f62b735f943b015af8c464ed1899bef271e1f
TheSAint <vishwa20@outlook.com>
2019-09-11 17:53:40 +0100

    jumper code added

--- HFunctions.py
+++ HFunctions.py
@@ -1,23 +1,54 @@
 import cv2
-import numpy
+import numpy as np
+import pdb
+import datetime
+
+
 
 background = None
+background_jump = None
+road_mask = cv2.imread('mask_count.jpg',cv2.IMREAD_GRAYSCALE)
+cross_mask = cv2.imread('mask_jumppoint.jpg"',cv2.IMREAD_GRAYSCALE)
 
-def get_averageBackground(weight, capture):
+
+def showimg(img):
+	cv2.imshow("asked",img)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
+
+def get_countBackground(weight, capture):
 	global background
 	for i in range(60):
 		ret,frame = capture.read()
 		gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+		if road_mask is not None:
+			gray = cv2.bitwise_and(gray,road_mask)
 		if background is None:
 			background = gray.copy().astype('float')
 			return None
 		cv2.accumulateWeighted(gray,background,weight)
 
+def get_jumpBackground(weight, capture):
+	global background_jump
+	for i in range(60):
+		ret,frame = capture.read()
+		gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+		if cross_mask is not None:
+			gray = cv2.bitwise_and(gray,cross_mask)
+		if background_jump is None:
+			background_jump = gray.copy().astype('float')
+			return None
+		cv2.accumulateWeighted(gray,background_jump,weight)
 
-def segment(gray,threshold_min = 25):
+
+
+def segment(gray, threshold_min = 25):
 	'''
 		Finds thresholds and contours for the grayscale segment
 	'''
+	#pdb.set_trace()
+	if road_mask is not None:
+		gray = cv2.bitwise_and(gray,road_mask)
 	diff = cv2.absdiff(background.astype('uint8'),gray)
 	ret,thresholded = cv2.threshold(diff,threshold_min,255,cv2.THRESH_BINARY)
 	cont,_ = cv2.findContours(thresholded.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -27,6 +58,25 @@ def segment(gray,threshold_min = 25):
 		segment = cont
 		return (thresholded,segment)
 
+
+def capture_jumpers(frame, gray, threshold_min = 25):
+	'''
+		Uses background subtractions to capture images of the jumper
+	'''
+	if cross_mask is not None:
+		gray = cv2.bitwise_and(gray,cross_mask)
+	diff = cv2.absdiff(background_jump.astype('uint8'),gray)
+	ret,thresholded = cv2.threshold(diff,threshold_min,255,cv2.THRESH_BINARY)
+	cont,_ = cv2.findContours(thresholded.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	if len(cont) is None:
+		return None
+	else:
+		for c in cont:
+			if cv2.contourArea(c) > 1000:
+				x,y,w,h = cv2.boundingRect(c)
+				cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),3)
+				jumper = frame[y:y+h,x:x+w]
+				cv2.imwrite('./jumpers/'+str(datetime.datetime.now())+'.jpg',jumper)
 
 
 def static_FGEx_count(gray):
@@ -48,16 +98,16 @@ def haarCascade_count(gray):
 		counts entities using haar classifiers
 	'''
 	#Creating classifier objects
-	car_cascade = cv2.CascadeClassifier(cars.xml)
-	bike_cascade = cv2.CascadeClassifier(two_wheeler.xml)
-	bus_cascade = cv2.CascadeClassifier(bus_front.xml)
+	car_cascade = cv2.CascadeClassifier("cars.xml")
+	bike_cascade = cv2.CascadeClassifier("two_wheeler.xml")
+	bus_cascade = cv2.CascadeClassifier("bus_front.xml")
 
 	#Detecting objects
 	cars = car_cascade.detectMultiScale(gray, 1.1, 2)
-    	bike = bike_cascade.detectMultiScale(gray,1.01, 1)
-	bus = bus_cascade.detectMultiScale(gray, 1.16, 1)
+	bike = bike_cascade.detectMultiScale(gray,1.01, 1)
+#	bus = bus_cascade.detectMultiScale(gray, 1.16, 1)
 
-	boundings = list(cars) + list(bike) + list(bus)
+	boundings = list(cars) + list(bike) #+ list(bus)
 	#Returns bounding rectangles and item_count
 	return boundings,len(boundings)				
 	
--- testrun.py
+++ testrun.py
@@ -2,11 +2,18 @@ from HFunctions import *
 import cv2
 import time
 import pdb
+import numpy as np
 
 ACC_WT = 0.5
 ALPHA = 3 #Reduction weight factor
+
 try:
+	cap = cv2.VideoCapture(0)
+	get_countBackground(ACC_WT,cap)
+	get_jumpBackground(ACC_WT,cap)
+	cap.release()
 	while True:
+
 		#GREEN ROUTINE
 		print("SIGNAL STATUS : GREEN")
 		time.sleep(5) #Waiting on green for 5 seconds
@@ -18,37 +25,49 @@ try:
 		RED_TIMER = 50
 		UPD_COUNT = 5
 		cap = cv2.VideoCapture(0)
-		try:
-			get_averageBackground(ACC_WT,cap)
-			while RED_TIMER > 0:
-				ret,frame = cap.read()
-				gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)		
-				rect,count = static_FGEx_count(gray)
-				cv2.putText(frame,str(RED_TIMER),(50,50),cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255) ,2)
-				for (x,y,w,h) in rect:
-					cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
+#		try:
 
-				#Timer update logic
-				if FRAME_COUNT % 25 == 0:
-					if UPD_COUNT > 0:
-						UPD_COUNT -= 1
-						RED_TIMER -= 1
-					else:
-						UPD_COUNT = 5
-						RED_TIMER = RED_TIMER - int(1 + ALPHA * count)
+		while RED_TIMER > 0:
+			ret,frame = cap.read()
+			pts = np.array([[490,0], [150,0] ,[60,300], [580,300]], np.int32)
+			jump_pnts = np.array([[60,310],[0,480], [640,480], [580,310] ], np.int32)
+			gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-				cv2.imshow("Live",frame)
-				FRAME_COUNT += 1
-				if cv2.waitKey(1) & 0xFF == ord('q'):
-					break	
 
-					
-			cap.release()
-			cv2.destroyAllWindows()	
+			#rect,count = haarCascade_count(gray)		
+			rect,count = static_FGEx_count(gray)
+			capture_jumpers(frame, gray)
 
-		except:
-			cap.release()
-			cv2.destroyAllWindows()	
+
+
+			cv2.putText(frame,str(RED_TIMER),(50,50),cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255) ,2)
+			cv2.polylines(frame, [pts], True,(0,255,0),3)
+			cv2.polylines(frame, [jump_pnts], True,(0,0,255),3)
+			for (x,y,w,h) in rect:
+				cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
+
+			#Timer update logic
+			if FRAME_COUNT % 25 == 0:
+				if UPD_COUNT > 0:
+					UPD_COUNT -= 1
+					RED_TIMER -= 1
+				else:
+					UPD_COUNT = 5
+					RED_TIMER = RED_TIMER - int(1 + ALPHA * count)
+
+			cv2.imshow("Live",frame)
+			FRAME_COUNT += 1
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break	
+
+				
+		cap.release()
+		cv2.destroyAllWindows()	
+
+#		except:
+#			cap.release()
+#			cv2.destroyAllWindows()
+
 
 except KeyboardInterrupt:
 	print("Terminated")
