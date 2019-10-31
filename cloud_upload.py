b65526b26a4deed1b873a8d1bff16537c1ace5f2
vivekray903 <41966135+vivekray903@users.noreply.github.com>
2019-11-01 01:00:18 +0530

    Some Comments required
    Made some little changes. Kindly look onto it.

--- cloud_upload.py
+++ cloud_upload.py
@@ -2,14 +2,14 @@
 from google.cloud import storage
 import os
 from datetime import date
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Downloads/rpitraffic-firebase-adminsdk-526ol-d331fc91c9.json"
+os.environ["GOOGLE_API_KEYS"]="/home/pi/Downloads/rpitraffic-firebase-adminsdk-526ol-d331fc91c9.json"
 
 DIRECTORY = "./jumpers/"
 def cloud_upload():
 
 	 
 	try: 
-		#Getting all the file names
+		# Getting all the file names
 		files = []
 		# r=root, d=directories, f = files
 		for r, d, f in os.walk(DIRECTORY):
@@ -20,16 +20,16 @@ def cloud_upload():
 		client = storage.Client()
 		# Reference an existing bucket.
 		bucket = client.get_bucket('rpitraffic.appspot.com')
-		#getting the date
+		# Getting the date and creating filename according to date and time fetched
 		today = date.today()
 		cloud_dir = str(today.strftime("%d-%m-%Y"))+"/"
 		for path,name in files:
-		# Upload a local file to a new file to be created in your bucket. 
+		# Upload a local file to the Created Cloud file in your bucket. 
 			Blob = bucket.blob(cloud_dir+name)
 			Blob.upload_from_filename(filename=path)
 			os.remove(path)
 		return 1
 	except:
-		#For faileurs
+		#For failures
 		return 0
 
